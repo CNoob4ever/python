e13ad505fdd1ebfecefa533d984557152e5a06d4
CNoob4ever <wustshihaishen@outlook.com>
2020-06-18 22:37:14 +0800

    remove analysis.py~

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -2,6 +2,7 @@
 depend on pandas xlrd
 '''
 import pandas as pd
+import matplotlib.pyplot as plt
 
 data = pd.read_excel('红手指游戏数据.xlsx')
 data['日期'] = [x.strftime('%Y-%m-%d') for x in data['游戏开始时间']]
@@ -19,6 +20,9 @@ for i in date:
     table = table.append(pd.DataFrame({'日期':i,'试玩次数':[count_per_day],'试玩时长':[duration],'最大试玩时长':[max_duration],'最小试玩时长':[min_duration],'平均时长':[mean_duration]}),ignore_index = True)
 
 print(table)
+
+table['试玩次数'].plot()
+plt.show()
 
 #print(data)
 
